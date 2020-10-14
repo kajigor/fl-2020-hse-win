e2a603a73bb4003c972d7703c0220bdd28adf950
GlebSolovev <lionyoung@mail.ru>
2020-10-15 01:27:28 +0300

    Setup and finish first part

--- parsecExample/src/PrologParser.hs
+++ parsecExample/src/PrologParser.hs
@@ -18,12 +18,12 @@ languageDef =
 
 lexer = Token.makeTokenParser languageDef
 
+identifier :: Parser String
 identifier = do
   i <- Token.identifier lexer
-  guard $ isLower $ head i
   return i
 
-var :: Parser [Char]
+var :: Parser String
 var = do
   h <- upper
   t <- many (alphaNum <|> char '_')
@@ -37,19 +37,75 @@ brackets = Token.parens lexer
 dot = Token.dot lexer
 
 atom :: Parser Atom
-atom = undefined
+atom = do
+  head <- identifier
+  args <- many parseAtomArg
+  return Atom {atomHead = head, atomArgs = args}
+
+parseAtomArg :: Parser (Either Atom String)
+parseAtomArg = 
+  (fmap Left (do
+  head <- identifier
+  return Atom {atomHead = head, atomArgs = []}
+                         )) <|>
+  (fmap Right var) <|>
+  (manyBrackets (fmap Left atom <|> fmap Right var))
+
+manyBrackets :: Parser a -> Parser a
+manyBrackets p = brackets (manyBrackets p) <|> p
+
 
 relation :: Parser Relation
-relation = undefined
+relation = do
+  head <- atom
+  body <- parseDisjOrNothing
+  dot
+  return Relation {relHead = head, relBody = body}
+
+parseDisjOrNothing :: Parser (Maybe RelationBody)
+parseDisjOrNothing = 
+  (fmap Just (do 
+    reservedOp ":-"
+    d <- parseDisj
+    return d
+             )) <|> 
+  (do; return Nothing)
+
+parseList :: Parser a -> Parser b -> Parser [a]
+parseList elem sep = do
+  h <- elem
+  t <- many (sep >> elem)
+  return (h:t)
+
+parseDisj :: Parser RelationBody
+parseDisj = fmap (foldr1 Disj) $ parseList parseConj (reservedOp ";")
+
+parseConj :: Parser RelationBody
+parseConj = fmap (foldr1 Conj) $ parseList parseBodyElem (reservedOp ",")
+
+parseBodyElem :: Parser RelationBody
+parseBodyElem = fmap RAtom atom <|> brackets parseDisj
 
 parseModule :: Parser String
-parseModule = undefined
+parseModule = do
+  reserved "module"
+  name <- identifier
+  dot
+  return name
 
 typeExpr :: Parser Type
-typeExpr = undefined
+typeExpr = fmap (foldr1 Arrow) $ parseList parseTypeElem (reservedOp "->")
+
+parseTypeElem :: Parser Type
+parseTypeElem = fmap TAtom atom <|> fmap Var var <|> brackets typeExpr
 
 typ :: Parser TypeDef
-typ = undefined
+typ = do
+  reserved "type"
+  name <- identifier
+  t <- typeExpr
+  dot
+  return $ TypeDef name t
 
 prog :: Parser PrologProgram
 prog = undefined
